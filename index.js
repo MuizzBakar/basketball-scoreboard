0373cc611333e880e2860c06d70744dcf07703f7
Abdulmuizz Abubakar <muizz.bakar@gmail.com>
2022-08-17 14:17:25 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+let homeScoreEl = document.querySelector("#home-score-el");
+let awayScoreEl = document.querySelector("#away-score-el");
+
+let homeCount = 0;
+let awayCount = 0;
+
+function oneHomePoint() {
+  homeCount += 1;
+  homeScoreEl.textContent = homeCount;
+}
+
+function twoHomePoints() {
+  homeCount += 2;
+  homeScoreEl.textContent = homeCount;
+}
+
+function threeHomePoints() {
+  homeCount += 3;
+  homeScoreEl.textContent = homeCount;
+}
+
+function oneAwayPoint() {
+  awayCount += 1;
+  awayScoreEl.textContent = awayCount;
+}
+
+function twoAwayPoints() {
+  awayCount += 2;
+  awayScoreEl.textContent = awayCount;
+}
+
+function threeAwayPoints() {
+  awayCount += 3;
+  awayScoreEl.textContent = awayCount;
+}
+
+function twoHomePoint() {
+  count += 2;
+  homeScoreEl.textContent = count;
+}
